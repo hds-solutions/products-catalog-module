f9f90c0aa0fab1025e316d70aec16ee38d834ca4
Hermann D. Schimpf <hschimpf@hds-solutinons.net>
2021-02-23 12:37:45 -0300

    Disable all for now

--- src/ProductsCatalogModuleServiceProvider.php
+++ src/ProductsCatalogModuleServiceProvider.php
@@ -13,31 +13,31 @@ class ProductsCatalogModuleServiceProvider extends ModuleServiceProvider {
 
     public function bootEnv():void {
         // enable config override
-        $this->publishes([
-            module_path('config/products-catalog.php') => config_path('products-catalog.php'),
-        ], 'products-catalog.config');
+        // $this->publishes([
+        //     module_path('config/products-catalog.php') => config_path('products-catalog.php'),
+        // ], 'products-catalog.config');
 
         // load routes
         $this->loadRoutesFrom( module_path('routes/products-catalog.php') );
         // load views
-        $this->loadViewsFrom( module_path('resources/views'), 'products-catalog' );
+        // $this->loadViewsFrom( module_path('resources/views'), 'products-catalog' );
         // load migrations
-        $this->loadMigrationsFrom( module_path('database/migrations') );
+        // $this->loadMigrationsFrom( module_path('database/migrations') );
         // load seeders
-        $this->loadSeedersFrom( module_path('database/seeders') );
+        // $this->loadSeedersFrom( module_path('database/seeders') );
     }
 
     public function register() {
-        // register helpers
-        if (file_exists($helpers = realpath(__DIR__.'/helpers.php')))
-            //
-            require_once $helpers;
+        // // register helpers
+        // if (file_exists($helpers = realpath(__DIR__.'/helpers.php')))
+        //     //
+        //     require_once $helpers;
         // register singleton
-        app()->singleton('products-catalog', fn() => new ProductsCatalog);
+        // app()->singleton('products-catalog', fn() => new ProductsCatalog);
         // register commands
         // $this->commands( $this->commands );
         // merge configuration
-        $this->mergeConfigFrom( module_path('config/products-catalog.php'), 'products-catalog' );
+        // $this->mergeConfigFrom( module_path('config/products-catalog.php'), 'products-catalog' );
     }
 
 }
